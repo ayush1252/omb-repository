a4ccbbc1c0face3aa469f07b9e7e7baa136f2fd6
FAREAST\ayaggarwal <ayaggarwal@microsoft.com>
2023-01-02 12:01:22 +0530

    Configuration for large message

--- dedicatedperf.sh
+++ dedicatedperf.sh
@@ -5,9 +5,11 @@ TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 echo $TIMESTAMP
 
 # AMQP
+bin/benchmark --drivers driver-azure-eventhubs/amqp-dedicated-v2.yaml workloads/1producer-1consumer-1MBMessage-20mins.yaml -o amqpdedicatedv2-1producer-1consumer-largemessage-20min
 bin/benchmark --drivers driver-azure-eventhubs/amqp-dedicated-v2.yaml workloads/1producer-1consumer-1Mbps-20mins.yaml -o amqpdedicatedv2-1producer-1consumer-1MBPS-20min
 
 #Kafka
+bin/benchmark --drivers driver-kafka/kafka-dedicated-v2.yaml workloads/1producer-1consumer-1MBMessage-20mins.yaml -o kafkadedicatedv2-1producer-1consumer-largemessage-20min
 bin/benchmark --drivers driver-kafka/kafka-dedicated-v2.yaml workloads/1producer-1consumer-1Mbps-20mins.yaml -o kafkadedicatedv2-1producer-1consumer-1MBPS-20min
 
 echo "Completed Execution of Benchmarking Tests"
--- premiumperf.sh
+++ premiumperf.sh
@@ -6,8 +6,10 @@ echo $TIMESTAMP
 
 # AMQP
 bin/benchmark --drivers driver-azure-eventhubs/amqp-premium.yaml workloads/1producer-1consumer-1Mbps-20mins.yaml -o amqp-1producer-1consumer-1MBPS-20min
+bin/benchmark --drivers driver-azure-eventhubs/amqp-premium.yaml workloads/1producer-1consumer-1MBMessage-20mins.yaml -o amqp-1producer-1consumer-largemessage-20min
 
 #Kafka
 bin/benchmark --drivers driver-kafka/kafka-premium.yaml workloads/1producer-1consumer-1Mbps-20mins.yaml -o kafka-1producer-1consumer-1MBPS-20min
+bin/benchmark --drivers driver-kafka/kafka-premium.yaml workloads/1producer-1consumer-1MBMessage-20mins.yaml -o kafka-1producer-1consumer-largemessage-20min
 
 echo "Completed Execution of Benchmarking Tests"
