b4cf386a1f03d163bc1933f3d8ccb7c0c8dafcc4
FAREAST\ayaggarwal <ayaggarwal@microsoft.com>
2023-05-15 19:38:21 +0530

    Minor fix to Error Handling

--- benchmark-framework/src/main/java/io/openmessaging/benchmark/Benchmark.java
+++ benchmark-framework/src/main/java/io/openmessaging/benchmark/Benchmark.java
@@ -213,6 +213,7 @@ public class Benchmark {
                     generator.close();
                 } catch (Exception e) {
                     log.error("Failed to run the workload '{}' for driver '{}'", workload.name, driverConfig, e);
+                    throw new RuntimeException(e);
                 } finally {
                     try {
                         worker.stopAll();
