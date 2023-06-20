bb31f9ba00d46725cfede2c73e2de90a450baa72
FAREAST\ayaggarwal <ayaggarwal@microsoft.com>
2023-06-20 15:52:09 +0530

    Minor Bug Fix

--- driver-api/src/main/java/io/openmessaging/benchmark/driver/NamespaceMetadata.java
+++ driver-api/src/main/java/io/openmessaging/benchmark/driver/NamespaceMetadata.java
@@ -1,10 +1,14 @@
 package io.openmessaging.benchmark.driver;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NamespaceMetadata implements Serializable {
     public String NamespaceName;
     public String SubscriptionId;
