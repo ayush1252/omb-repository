9272296b1d4ec8af807ecb039ac38321b90ae914
FAREAST\ayaggarwal <ayaggarwal@microsoft.com>
2023-06-20 15:45:57 +0530

    Adding support for Builder

--- driver-api/src/main/java/io/openmessaging/benchmark/driver/NamespaceMetadata.java
+++ driver-api/src/main/java/io/openmessaging/benchmark/driver/NamespaceMetadata.java
@@ -1,7 +1,10 @@
 package io.openmessaging.benchmark.driver;
 
+import lombok.Builder;
+
 import java.io.Serializable;
 
+@Builder
 public class NamespaceMetadata implements Serializable {
     public String NamespaceName;
     public String SubscriptionId;
